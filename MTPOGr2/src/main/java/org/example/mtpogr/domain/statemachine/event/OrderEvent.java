3e697abc30a990ecebd13433fde0b34e6c36f93b
GrandForStu <qeouty@gmail.com>
2026-09-22 23:03:09 +0300

    Remove unnecessary folder

--- MTPOGr2/src/main/java/org/example/mtpogr/domain/statemachine/event/OrderEvent.java
+++
@@ -1,5 +0,0 @@
-package org.example.mtpogr.domain.statemachine.event;
-
-public enum OrderEvent {
-    CONFIRM, PAY, SHIP, DELIVER, CANCEL
-}
--- MTPOGr2/src/main/java/org/example/mtpogr/domain/statemachine/state/OrderState.java
+++
@@ -1,5 +0,0 @@
-package org.example.mtpogr.domain.statemachine.state;
-
-public enum OrderState {
-    NEW, CONFIRMED, PAID, SHIPPED, DELIVERED, CANCELLED
-}
